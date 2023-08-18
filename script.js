669c2bf8817fbc4d8416566bcd86cdf9107148bd
meanwhile7 <abhijit793001@gmail.com>
2023-08-18 12:44:29 +0530

    revamped code and moduled code

--- script.js
+++ script.js
@@ -1,189 +1,137 @@
 const openModal = document.querySelector(".newbook");
 const modal = document.querySelector("#modal");
 const closemodal = document.querySelector(".closemodal");
-const gridbook = document.querySelector(".gridbook");
 const addButton = document.querySelector(".add-book");
-const reset = document.querySelector("#storbutton");
-const book = document.querySelector(".book_row");
 const clear = document.querySelector(".clear");
 
+// Attach event listeners
 openModal.addEventListener("click", () => {
-  // Add the event listener to the "Add" button when the dialog opens
   addButton.addEventListener("click", submitForm);
-  clear.addEventListener("click", clearfield);
+  clear.addEventListener("click", clearField);
   modal.showModal();
 });
 
-closemodal.addEventListener("click", () => {
-  addButton.removeEventListener("click", submitForm);
-  clear.removeEventListener("click", clearfield);
-  modal.close();
-});
+closemodal.addEventListener("click", closeModal);
 
-reset.addEventListener("click", () => {
+// Clear localStorage
+document.querySelector("#storbutton").addEventListener("click", () => {
   localStorage.clear();
 });
 
-// Add an event listener to the "Add" button
 function submitForm() {
-  // Retrieve input values from the form fields
-  const title = document.getElementById("b-title").value;
-  const author = document.getElementById("b-author").value;
-  const language = document.getElementById("b-language").value;
-  const totalPages = parseInt(document.getElementById("b-nbr_of_pages").value);
-  const pagesRead = parseInt(
-    document.getElementById("b-nbr_of_pages_read").value
-  );
-  const readStatus = document.getElementById("b-read_status").value;
+  const bookData = extractFormData();
+  if (!validateFormData(bookData)) return;
+  if (isDuplicateBook(bookData)) return;
+  storeBookData(bookData);
+  clearField();
+  addBook(bookData);
+}
 
-  // Check if any of the input fields are blank or null
+function closeModal() {
+  addButton.removeEventListener("click", submitForm);
+  clear.removeEventListener("click", clearField);
+  modal.close();
+}
+
+function extractFormData() {
+  return {
+    title: document.getElementById("b-title").value,
+    author: document.getElementById("b-author").value,
+    language: document.getElementById("b-language").value,
+    totalPages: parseInt(document.getElementById("b-nbr_of_pages").value),
+    pagesRead: parseInt(document.getElementById("b-nbr_of_pages_read").value),
+    readStatus: document.getElementById("b-read_status").value === "true",
+  };
+}
+
+function validateFormData(bookData) {
+  const { title, author, language, totalPages, pagesRead } = bookData;
   if (!title || !author || !language || isNaN(totalPages) || isNaN(pagesRead)) {
     alert("Please fill in all the required fields.");
-    return;
+    return false;
   }
+  return true;
+}
 
-  // Create a unique identifier for each book (using title and author)
-  const bookIdentifier = `${title}-${author}`;
-  // Retrieve the existing data from local storage
-  const storedDataJSON = localStorage.getItem("myData");
-  let storedDataArray = [];
-
-  if (storedDataJSON) {
-    storedDataArray = JSON.parse(storedDataJSON);
-  }
-
-  // Create a Set from the stored data's unique identifiers
+function isDuplicateBook(bookData) {
+  const storedDataArray = getStoredDataArray();
+  const bookIdentifier = `${bookData.title}-${bookData.author}`;
   const existingIdentifiers = new Set(
-    storedDataArray.map(
-      (existingBook) =>
-        `${existingBook["book-title"]}-${existingBook["book-author"]}`
-    )
+    storedDataArray.map(book => `${book["book-title"]}-${book["book-author"]}`)
   );
-
-  // Check if the entered book's identifier is already in the Set
   if (existingIdentifiers.has(bookIdentifier)) {
     alert("Duplicate book data. This book is already added.");
-    return; // Exit the function if it's a duplicate
+    return true;
   }
+  return false;
+}
 
-  // Create an object to store the form data
-  const bookData = {
-    "book-title": title,
-    "book-author": author,
-    "book-language": language,
-    "pages-count": totalPages,
-    "pages-read": pagesRead,
-    "book-status": readStatus === "true",
-  };
+function getStoredDataArray() {
+  return JSON.parse(localStorage.getItem("myData")) || [];
+}
 
-  // Retrieve the existing data from local storage
-
-  if (storedDataJSON) {
-    storedDataArray = JSON.parse(storedDataJSON);
-  }
-
-  // Add the new book data to the existing array
+function storeBookData(bookData) {
+  const storedDataArray = getStoredDataArray();
   storedDataArray.push(bookData);
+  localStorage.setItem("myData", JSON.stringify(storedDataArray));
+}
 
-  // Store the updated data back into local storage
-  const updatedDataArrayJSON = JSON.stringify(storedDataArray);
-  localStorage.setItem("myData", updatedDataArrayJSON);
-
+function clearField() {
   document.getElementById("b-title").value = "";
   document.getElementById("b-author").value = "";
   document.getElementById("b-language").value = "";
   document.getElementById("b-nbr_of_pages").value = "";
   document.getElementById("b-nbr_of_pages_read").value = "";
-
-  // creating element
-
-  // create_element(bookData)
-
-  add_book(bookIdentifier);
 }
 
-function add_book(bookIdentifier) {
-  // Retrieve the stored data from local storage using the identifier
-  const storedDataJSON = localStorage.getItem("myData");
-  const storedDataArray = JSON.parse(storedDataJSON);
-  console.log(storedDataJSON);
-  console.log(storedDataArray[1]);
-  // Find the book data using the identifier
-  const bookData = storedDataArray.find((book) => {
-    const identifier = `${book["book-title"]}-${book["book-author"]}`;
-    return identifier === bookIdentifier;
-  });
-
-  if (!bookData) {
-    console.error("Book data not found.");
-    return;
-  }
-
-  // Create elements for each book and append them to the 'book' element
+function addBook(bookData) {
   const bookElement = document.querySelector(".book_row");
-  const newBookDetails = document.createElement("div");
-  newBookDetails.classList.add("book-details"); // You can adjust the class name as needed
-
-  // Extract data from the retrieved bookData
-  const title = bookData["book-title"];
-  const author = bookData["book-author"];
-  const language = bookData["book-language"];
-  const totalPages = bookData["pages-count"];
-  const pagesRead = bookData["pages-read"];
-  const readStatus = bookData["book-status"];
-
-  // Create and append individual elements
-  const h3Element = document.createElement("h3");
-  h3Element.textContent = title;
-  newBookDetails.appendChild(h3Element);
-
-  const authorElement = document.createElement("span");
-  authorElement.className = "book-author";
-  authorElement.textContent = `Author: ${author}`;
-  newBookDetails.appendChild(authorElement);
-
-  const languageElement = document.createElement("span");
-  languageElement.className = "book-lang";
-  languageElement.textContent = `Language: ${language}`;
-  newBookDetails.appendChild(languageElement);
-
-  const totalPagesElement = document.createElement("span");
-  totalPagesElement.className = "tot-page";
-  totalPagesElement.textContent = `Total Pages: ${totalPages}`;
-  newBookDetails.appendChild(totalPagesElement);
-
-  const pagesReadElement = document.createElement("span");
-  pagesReadElement.className = "page-read";
-  pagesReadElement.textContent = `Pages Read: ${pagesRead}`;
-  newBookDetails.appendChild(pagesReadElement);
-
-  const readStatusElement = document.createElement("span");
-  readStatusElement.className = "read-toggle";
-  readStatusElement.textContent = `Read Status: ${readStatus}`;
-  newBookDetails.appendChild(readStatusElement);
-
-  const labelContainer = document.createElement("label");
-  labelContainer.className = "toggle-container";
-  newBookDetails.appendChild(labelContainer);
-
-  const toggleInput = document.createElement("input");
-  toggleInput.type = "checkbox";
-  toggleInput.className = "toggle-input";
-  labelContainer.appendChild(toggleInput);
-
-  const toggleSlider = document.createElement("span");
-  toggleSlider.className = "toggle-slider";
-  labelContainer.appendChild(toggleSlider);
-
-  // Append the new book element to the 'book' container
+  const newBookDetails = createBookDetailsElement(bookData);
   bookElement.appendChild(newBookDetails);
 }
 
-function clearfield() {
-  const test = (document.getElementById("b-title").value = "");
-  console.log(test);
-  document.getElementById("b-author").value = "";
-  document.getElementById("b-language").value = "";
-  document.getElementById("b-nbr_of_pages").value = "";
-  document.getElementById("b-nbr_of_pages_read").value = "";
+function createBookDetailsElement(bookData) {
+  const newBookDetails = document.createElement("div");
+  newBookDetails.classList.add("book-details");
+
+  const { title, author, language, totalPages, pagesRead, readStatus } = bookData;
+
+  const deleteButton = document.createElement("span");
+  deleteButton.className = "remove-book";
+  newBookDetails.appendChild(deleteButton);
+
+  const elementsToCreate = [
+    { tag: "h3", text: title },
+    { tag: "span", className: "book-author", text: `Author: ${author}` },
+    { tag: "span", className: "book-lang", text: `Language: ${language}` },
+    { tag: "span", className: "tot-page", text: `Total Pages: ${totalPages}` },
+    { tag: "span", className: "page-read", text: `Pages Read: ${pagesRead}` },
+    { tag: "span", className: "read-toggle", text: `Mark as read: ${readStatus}` },
+    { tag: "label", className: "toggle-container", children: [
+      { tag: "input", className: "toggle-input", type: "checkbox" },
+      { tag: "span", className: "toggle-slider" }
+    ] }
+  ];
+
+  elementsToCreate.forEach(elementConfig => {
+    const element = document.createElement(elementConfig.tag);
+    if (elementConfig.className) {
+      element.className = elementConfig.className;
+    }
+    if (elementConfig.text) {
+      element.textContent = elementConfig.text;
+    }
+    if (elementConfig.children) {
+      elementConfig.children.forEach(childConfig => {
+        const child = document.createElement(childConfig.tag);
+        if (childConfig.className) {
+          child.className = childConfig.className;
+        }
+        element.appendChild(child);
+      });
+    }
+    newBookDetails.appendChild(element);
+  });
+
+  return newBookDetails;
 }
